e96ea868b7451a006d281d8ed51b1b163dd8384b
m-popovic <maja.popovic.166@gmail.com>
2017-06-14 13:20:50 +0200

    Add files via upload

---
+++ example.hyp.cs
@@ -0,0 +1,10 @@
+Nedávné nabídky evakuace tvoří režim a Rusko znělo jako slabě zahalené výhrůžky, uvedli chirurgové pastevci a další lékaři.
+Lékaři uvedli, že minulý měsíc došlo k 42 útokům na zdravotnická zařízení v Sýrii, 15 z nich byly nemocnice, ve kterých pracují.
+Co nás nejvíc trápí, protože lékaři si vybírají, kdo bude žít a kdo zemře.
+Malé děti jsou někdy přivedeny do našich nouzových pokojů tak těžce raněných, že musíme upřednostnit ty, kteří mají lepší šance,
+Nebo prostě nemají vybavení, které by jim pomohlo, uvedli lékaři.
+Před dvěma týdny čtyři novorozenci, kteří lapali po dechu, se udusili k smrti poté, co výbuch snížil přísun kyslíku do jejich inkubátorů.
+Lapali po dechu, jejich životy skončily dřív, než skutečně začaly.
+V posledních týdnech síly věrné Bašáru Asadovi podporované ruskými silami ovládly povstalecké východní Aleppo s přes 200 000 lidí stále uvězněných uvnitř bez dodávek potravin.
+Povstalcům, kteří jsou pod neustálým útokem ruského a syrského letectva, se v sobotu podařilo prolomit obklíčení a zahájit protiofenzivu.
+Obě strany přidaly posily jak ve městě, tak v okolních oblastech.
---
+++ example.ref.cs
@@ -0,0 +1,10 @@
+Nedávné nabídky evakuace obyvatel od syrského režimu a Ruska zní jako jen lehce zastřené výhrůžky, podotkli pediatři, chirurgové a další lékaři.
+Lékaři uvedli, že za uplynulý měsíc bylo zaznamenáno 42 útoků na lékařská zařízení v Sýrii, z toho 15 na nemocnice, ve kterých pracují.
+Nejvíce smutní jsme z toho, že musíme rozhodovat o tom, kdo bude žít a kdo zemře.
+Na pohotovost k nám přicházejí malé děti s tak vážnými zraněními, že musíme mezi nimi vybírat ty, u nichž je největší pravděpodobnost, že přežijí.
+A někdy nemáme ani potřebný materiál, abychom jim pomohli, popsali lékaři.
+Před dvěma týdny se udusili čtyři novorozenci, když výbuch přerušil dodávky kyslíku do jejich inkubátorů.
+Lapali po dechu a pak jejich život skončil - dřív, než skutečně mohl začít, připomněli.
+Armáda věrná prezidentovi Bašáru Asadovi, kterou podporují ruské síly, v minulých týdnech obklíčila povstalci ovládanou východní část Halabu, uvnitř se ocitlo bez dodávek potravin přes 200.000 lidí.
+Rebelům, kteří jsou pod neustálými údery ruského a syrského letectva, se v sobotu po třech týdnech podařilo obklíčení prolomit a zahájit protiofenzivu.
+Oběma stranám poté přibyly ve městě a jeho okolí posily.
